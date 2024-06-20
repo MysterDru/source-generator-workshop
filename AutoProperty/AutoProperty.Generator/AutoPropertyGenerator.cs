96ff0c13cd439459c0dbca0cd080f97d96f70e30
Drew Frisk <dfrisk@metafarms.com>
2024-06-20 18:44:12 -0500

    Exercise 1
    Initial solution, project and file creation for exercise 1

---
+++ AutoProperty/AutoProperty.Generator/AutoPropertyGenerator.cs
@@ -0,0 +1,12 @@
+using Microsoft.CodeAnalysis;
+
+namespace AutoProperty.Generator;
+
+[Generator]
+public class AutoPropertyGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // todo: implement the generator
+    }
+}
